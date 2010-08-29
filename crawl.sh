12e2bffd76d926c35a533e79c40dd513d9111815
ceteri <ceteri@gmail.com>
2010-08-29 02:18:04 -0700

    clean up

--- crawl.sh
+++ crawl.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -x
 
-#host=172.16.100.63
 host=localhost
 
 ./src/consume.py $host:6379:0 flush
-./src/consume.py $host:6379:0 config < imvu_config.tsv 
-./src/consume.py $host:6379:0 whitelist < imvu_white.tsv 
-./src/consume.py $host:6379:0 seed < imvu_seed.tsv 
+./src/consume.py $host:6379:0 config < config.tsv 
+./src/consume.py $host:6379:0 whitelist < white.tsv 
+./src/consume.py $host:6379:0 seed < seed.tsv 
 ./src/consume.py $host:6379:0 perform
 
-#./src/consume.py $host:6379:0 stopwords < stopwords
+#./src/consume.py $host:6379:0 persist
+#./src/consume.py $host:6379:0 analyze
+
+
